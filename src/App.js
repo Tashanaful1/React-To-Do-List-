f20dcf500ed75319350615fd3e757f76df25af0f
Alex <rollinglex@gmail.com>
2020-08-05 17:03:23 -0500

    alex changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+function App () {
+  return <div className='App'>Hello Alex!</div>
 }
 
-export default App;
+export default App
